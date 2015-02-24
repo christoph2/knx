a42ea62866519bda4a932aeca82a30678bd4667b
Christoph Schueler <cpu12.gems@googlemail.com>
2015-02-24 12:09:14 +0100

    mixins.py: Added class 'ApplicationMixin'.

--- knxReTk/vdimex/xmlloader/mixins.py
+++ knxReTk/vdimex/xmlloader/mixins.py
@@ -4,7 +4,7 @@
 __copyright__ = """
    Konnex / EIB Reverserz Toolkit
 
-   (C) 2001-2014 by Christoph Schueler <cpu12.gems@googlemail.com>
+   (C) 2001-2015 by Christoph Schueler <cpu12.gems@googlemail.com>
 
    All Rights Reserved
 
@@ -28,7 +28,7 @@ __author__  = 'Christoph Schueler'
 __version__ = '0.1.0'
 
 
-from copy import copy
+toBoolean = lambda v: True if (v == '1' or v == 'true') else False
 
 class BaseMixin(object):
 
@@ -37,7 +37,7 @@ class BaseMixin(object):
     def convertAttributes(self, attrs):
         for k, v in attrs.items():
             if k.startswith('is') or k.startswith('has') and k != 'hash':
-                attrs[k] = True if v == '1' else False
+                attrs[k] = toBoolean(v)
         if 'id' in attrs:
             _id = attrs.pop('id')
             attrs['_id'] = _id  # Make identifier usable by MongoDB.
@@ -93,27 +93,30 @@ class HardwareMixin(BaseMixin):
             #print "*** NO hardware2Program!!!",
             #print " " * self.level, "<", self.level, self.tags[-1], attrs
 
-from pprint import pprint
-
 
 class CatalogMixin(BaseMixin):
-    catalogLevel = 0
-    result = {"sections": [], "items": []}
-    current = result
-    currentItem = None
 
+    def __init__(self):
+        self.catalogLevel = 0
+        self.result = {"sections": [], "items": []}
+        self.stack = []
+        self.currentItem = self.result
+        self.stack.append(self.currentItem)
+        
     def onCatalogSectionStart(self, name, attrs):
         self.catalogLevel += 1
         attrs = self.convertAttributes(attrs)
         self.convert(attrs, 'nonRegRelevantDataVersion', int)
-        self.current.update(attrs)
-        self.currentItem = self.current
+        self.currentItem.update(attrs)
         newSection = {"sections": [], "items": []}
-        self.current["sections"].append(newSection)
-        self.current = newSection
+        self.currentItem['sections'].append(newSection)
+        self.currentItem = newSection
+        self.stack.append(self.currentItem)
 
     def onCatalogSectionEnd(self, name):
         self.catalogLevel -= 1
+        self.currentItem = self.stack.pop()
+#        self.currentItem = self.stack[-1]
 
     def onCatalogItemStart(self, name, attrs):
         attrs = self.convertAttributes(attrs)
@@ -149,4 +152,228 @@ class LanguageMixin(BaseMixin):
         self.translations.setdefault(self.translationUnit, {})
         self.translations[self.translationUnit].setdefault(self.translationElement, {})
         self.translations[self.translationUnit][self.translationElement].setdefault(self.languageIdentifier, []).append(attrs)
+
+
+class ApplicationMixin(BaseMixin):
+
+    def __init__(self):
+        self.manufacturers = {}
+        self.currentManufacturer = None
+        self.currentSegment = None
+        self.currentApplication = None
+        self.currentParameterType = None
+        self.currentFixup = None
+        self.result = self.manufacturers
+
+    def onManufacturerStart(self, name, attrs):
+        if attrs['refId'] not in self.manufacturers:
+            self.currentManufacturer = {'_id': attrs['refId'], 'applicationPrograms': []}
+            self.manufacturers[attrs['refId']] = self.currentManufacturer
+        else:
+            #print "HMMM", attrs, self.manufacturers
+            #self.manufacturer[sattrs['refId']]
+            if not self.currentManufacturer:
+                self.currentManufacturer = self.manufacturers[attrs['refId']]
+        #print "MANUFACTURER: ", self.currentManufacturer
+
+    def onApplicationProgramStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+
+        self.convert(attrs, 'applicationVersion', int)
+        self.convert(attrs, 'applicationNumber', int)
+        self.convert(attrs, 'peiType', int)
+        self.convert(attrs, 'additionalAddressesCount', int)
+        self.convert(attrs, 'nonRegRelevantDataVersion', int)
+        #self.convert(attrs, '', int)
+        self.convert(attrs, 'dynamicTableManagement', toBoolean)
+        self.convert(attrs, 'linkable', toBoolean)
+        self.convert(attrs, 'preEts4Style', toBoolean)
+        self.convert(attrs, 'convertedFromPreEts4Data', toBoolean)
+        self.convert(attrs, 'downloadInfoIncomplete', toBoolean)
+        self.convert(attrs, 'createdFromLegacySchemaVersion', toBoolean)
+        self.convert(attrs, 'broken', toBoolean)
+        attrs['static'] = {'code': {"segments": []}, 'parameterTypes': [], 'parameters': [],
+            'loadProcedures': [],"options": None, 'fixups': [], 'addressTable': {}, 'associationTable': {},
+            'extension': {}, 'comObjectTable': {'comObjects': []}
+        }
+        #attrs['static']['code'] = {"segments": []}
+        attrs['dynamic'] = []
+        #self.convert(attrs, '', toBoolean)
+        self.currentApplication = attrs
+        self.currentManufacturer['applicationPrograms'].append(attrs)
+        #print "App: ", attrs
+
+    def onOptionsStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'preferPartialDownloadIfApplicationLoaded', toBoolean)
+        self.convert(attrs, 'easyCtrlModeModeStyleEmptyGroupComTables', toBoolean)
+        self.convert(attrs, 'setObjectTableLengthAlwaysToOne', toBoolean)
+        self.convert(attrs, 'textParameterZeroTerminate', toBoolean)
+        self.convert(attrs, 'legacyNoPartialDownload', toBoolean)
+        self.convert(attrs, 'legacyNoMemoryVerifyMode', toBoolean)
+        self.convert(attrs, 'legacyNoOptimisticWrite', toBoolean)
+        self.convert(attrs, 'comparable', toBoolean)
+        self.convert(attrs, 'reconstructable', toBoolean)
+        self.convert(attrs, 'legacyDoNotReportPropertyWriteErrors', toBoolean)
+        self.convert(attrs, 'legacyNoBackgroundDownload', toBoolean)
+        self.convert(attrs, 'legacyDoNotCheckManufacturerId', toBoolean)
+        self.convert(attrs, 'legacyAlwaysReloadAppIfCoVisibilityChanged', toBoolean)
+        self.convert(attrs, 'legacyNeverReloadAppIfCoVisibilityChanged', toBoolean)
+        self.convert(attrs, 'legacyDoNotSupportUndoDelete', toBoolean)
+        self.convert(attrs, 'legacyAllowPartialDownloadIfAp2Mismatch', toBoolean)
+        self.convert(attrs, 'legacyKeepObjectTableGaps', toBoolean)
+        self.convert(attrs, 'partialDownloadOnlyVisibleParameters', toBoolean)
+        self.convert(attrs, 'legacyProxyCommunicationObjects', toBoolean)
+        self.convert(attrs, 'deviceInfoIgnoreRunState', toBoolean)
+        self.convert(attrs, 'deviceInfoIgnoreLoadedState', toBoolean)
+        self.convert(attrs, 'deviceCompareAllowCompatibleManufacturerId', toBoolean)
+        self.convert(attrs, 'lineCoupler0912NewProgrammingStyle', toBoolean)
+        self.currentApplication['static']['options'] = attrs
+
+    def onAddressTableStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'offset', int)
+        self.convert(attrs, 'maxEntries', int)
+        self.currentApplication['static']['addressTable'] = attrs
+
+    def onAssociationTableStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'offset', int)
+        self.convert(attrs, 'maxEntries', int)
+        self.currentApplication['static']['associationTable'] = attrs
+
+    def onFixupStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        attrs['offsets'] = []
+        self.currentFixup = attrs
+        self.currentApplication['static']['fixups'].append(self.currentFixup)
+
+    def onOffsetEnd(self, name):
+        self.currentFixup['offsets'].append(int(self.currentElement['textContent']))
+
+    def onAbsoluteSegmentStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'address', int)
+        self.convert(attrs, 'size', int)
+        self.convert(attrs, 'userMemory', int)
+        attrs['data'] = None
+        attrs['mask'] = None
+        attrs['type'] = "absolute"
+        self.currentSegment = attrs
+        self.currentApplication['static']['code']["segments"].append(attrs)
+
+    def onRelativeSegmentStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'offset', int)
+        self.convert(attrs, 'size', int)
+        self.convert(attrs, 'loadStateMachine', int)
+        attrs['type'] = "relative"
+        #attrs['data'] = None
+        #attrs['mask'] = None
+        self.currentSegment = attrs
+        self.currentApplication['static']['code']["segments"].append(attrs)
+
+    def onDataEnd(self, name):
+        self.currentSegment['data'] = self.currentElement['textContent']
+
+    def onMaskEnd(self, name):
+        self.currentSegment['mask'] = self.currentElement['textContent']
+
+    def onComObjectTableStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'offset', int)
+        self.currentApplication['static']['comObjectTable'].update(attrs)
+
+    def onComObjectStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'number', int)
+        self.currentApplication['static']['comObjectTable']['comObjects'].append(attrs)
+
+    def onParameterTypeStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.currentParameterType = attrs
+        self.currentApplication['static']['parameterTypes'].append(attrs)
+
+    def onTypeNumberStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'sizeInBit', int)
+        self.convert(attrs, 'minInclusive', int)
+        self.convert(attrs, 'maxInclusive', int)
+        self.currentParameterType['type'] = "number"
+        self.currentParameterType.update(attrs)
+
+    def onTypeTextStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'sizeInBit', int)
+        self.currentParameterType['type'] = "text"
+        self.currentParameterType.update(attrs)
+
+    def onTypeNoneStart(self, name, attrs):
+        self.currentParameterType['type'] = None
+
+    def onTypeRestrictionStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'sizeInBit', int)
+        attrs['values'] = []
+        self.currentParameterType['type'] = "enumeration"   # ???
+        self.currentParameterType['restriction'] = attrs
+
+    def onEnumerationStart(self, name, attrs):
+        attrs = self.convertAttributes(attrs)
+        self.convert(attrs, 'displayOrder', int)
+        self.convert(attrs, 'value', int)
+        self.currentParameterType['restriction']['values'].append(attrs)
+
+    def addLoadControl(self, attrs, type_, converter = {}):
+        attrs = self.convertAttributes(attrs)
+        for k, v in converter.items():
+            self.convert(attrs, k, v)
+        attrs['type'] = type_
+        self.currentApplication['static']['loadProcedures'].append(attrs)
+
+    def onLdCtrlConnectStart(self, name, attrs):
+        self.addLoadControl(attrs, 'connect')
+
+    def onLdCtrlUnloadStart(self, name, attrs):
+        self.addLoadControl(attrs, 'unload', {'lsmIdx': int})
+
+    def onLdCtrlLoadStart(self, name, attrs):
+        self.addLoadControl(attrs, 'load', {'lsmIdx': int})
+
+    def onLdCtrlAbsSegmentStart(self, name, attrs):
+        self.addLoadControl(attrs, 'absSegment', {
+            'lsmIdx': int, 'segType': int, 'address': int, 'size': int, 'access': int, 'memType': int, 'segFlags': int}
+        )
+
+    def onLdCtrlTaskSegmentStart(self, name, attrs):
+        self.addLoadControl(attrs, 'taskSegment', {'lsmIdx': int, 'address': int})
+
+    def onLdCtrlLoadCompletedStart(self, name, attrs):
+        self.addLoadControl(attrs, 'loadCompleted', {'lsmIdx': int})
+
+    def onLdCtrlRestartStart(self, name, attrs):
+        self.addLoadControl(attrs, 'restart')
+
+    def onLdCtrlDisconnectStart(self, name, attrs):
+        self.addLoadControl(attrs, 'disconnect')
+
+    def onLdCtrlComparePropStart(self, name, attrs):
+        self.addLoadControl(attrs, 'ldCtrlCompareProp', {"objIdx": int, "propId": int})
+
+    def onLdCtrlTaskPtrStart(self, name, attrs):
+        self.addLoadControl(attrs, 'ldCtrlTaskPtr', {'lsmIdx': int, 'initPtr': int, 'savePtr': int, 'serialPtr': int})
+
+    def onLdCtrlTaskCtrl2Start(self, name, attrs):
+        self.addLoadControl(attrs, 'ldCtrlTaskCtrl2', {'lsmIdx': int, 'callback': int, 'address': int, 'seg0': int, 'seg1': int})
+
+    def onLdCtrlTaskCtrl1Start(self, name, attrs):
+        self.addLoadControl(attrs, 'ldCtrlTaskCtrl1', {'lsmIdx': int, 'address': int, 'count': int})
+        """
+         (9, u'LdCtrlDelay'),
+         (9, u'LdCtrlLoadImageProp'),
+         (9, u'LdCtrlRelSegment'),
+         (9, u'LdCtrlWriteMem'),
+         (9, u'LdCtrlWriteProp'),
+         (9, u'LdCtrlWriteRelMem'),
+        """
 
