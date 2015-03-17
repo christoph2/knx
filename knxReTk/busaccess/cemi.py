cc01a972486d94c990cd9aebb6dd489a948566a9
Christoph Schueler <cpu12.gems@googlemail.com>
2015-03-17 15:13:33 +0100

    Added 'cemi.py'.

---
+++ knxReTk/busaccess/cemi.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__copyright__ = """
+   Konnex / EIB Reverserz Toolkit
+
+   (C) 2001-2015 by Christoph Schueler <cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+   s. FLOSS-EXCEPTION.txt
+"""
+__author__  = 'Christoph Schueler'
+__version__ = '0.1.0'
+
+
+L_RAW_IND           = 0x2D
+L_RAW_REQ           = 0x10
+L_RAW_CON           = 0x2F
+L_DATA_REQ          = 0x11
+L_DATA_CON          = 0x2E
+L_DATA_IND          = 0x29
+L_POLL_DATA_REQ     = 0x13
+L_POLL_DATA_CON     = 0x25
+M_PROP_READ_REQ     = 0xFC
+M_PROP_READ_CON     = 0xFB
+M_PROP_WRITE_REQ    = 0xF6
+M_PROP_WRITE_CON    = 0xF5
+M_PROP_INFO_IND     = 0xF7
+M_RESET_REQ         = 0xF1
+M_RESET_IND         = 0xF0
+
